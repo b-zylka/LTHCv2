6baf6161279e789544307e24b986cfc03a32c83a
Brandon Zylka <brandon.zylka@gmail.com>
2016-04-01 15:15:32 -0500

    Update branding.php

--- config/branding.php
+++ config/branding.php
@@ -1,8 +1,8 @@
 <?php
-//Edit this line to specify your logo, site title, and company name.
-define("APP_TITLE", "Marco, Inc.");
+// Edit this line to specify your logo, site title, and company name.
+define("APP_TITLE", "Marco Technologies, LLC.");
 define("LOGO", "images/logox40.png");
-define("COMPANY_NAME", "Marco, Inc.");
+define("COMPANY_NAME", "Marco Technologies, LLC.");
 define("LOGO_LINK", "www.marconet.com");
 define("LTURL", "labtech.domain.com");
 define("CWURL", "cw.domain.com");
@@ -10,11 +10,11 @@ define("CWCOID", "marco"); //ConnectWise Company ID
 
 // Edit js/rssFeeds.js line 13 and update to your local news RSS feed.
 
-//Edit this line please,  good WOEID to find your weather location ID
+// Edit this line please,  good WOEID to find your weather location ID
 define("WOEID", "12782393");
 define("CITYSTATE", "Saint Cloud ,MN");
 
-//Edit this line for your Lat/Longitude  for google maps
+// Edit this line for your Lat/Longitude  for google maps
 define("LAT", "45.5586842");
 define("LON", "-94.2059143");
 ?>
